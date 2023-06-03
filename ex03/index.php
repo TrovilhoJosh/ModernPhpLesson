bf1116ea37963e2cb75e76bb7220d9e2df6147ce
TrovilhoJosh <78884647+TrovilhoJosh@users.noreply.github.com>
2023-06-02 22:57:19 -0300

    php lessons

---
+++ ex03/index.php
@@ -0,0 +1,18 @@
+<?php 
+  $nome = "Josh";
+  $sobrenome= "Trovilho";
+  const PAIS = "Brasil";
+  //
+  $nome = "Rafael";
+  $sobrenome = "Vieira";
+
+  #PAIS = "EUA";
+
+  echo "Muito prazer, $nome $sobrenome! Voce mora no " . PAIS;
+
+  echo "Esse jeito de chamar uma variavel esta errado: $NOME";
+  //
+  echo "Esse jeito de chamar uma variavel esta errado: $Nome";
+  //
+  echo "Esse jeito de chamar uma variavel esta certo: $nome";
+?>
