950a9815bb8b40b2884006f135ed898f8a30c348
TrovilhoJosh <78884647+TrovilhoJosh@users.noreply.github.com>
2023-06-05 19:31:14 -0300

    update exs

--- ex05/index.php
+++ ex05/index.php
@@ -4,9 +4,21 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  <title>Intercao com formularios</title>
 </head>
 <body>
-  
+  <header>
+    <h1>Apresente-se para nos</h1>
+  </header>
+  <section>
+    <form action="cad.php" method="get">
+      <label for="nome">Nome:</label>
+      <input type="text" name="nome" id="idNome">
+      <label for="sobrenome">Sobrenome:</label>
+      <input type="text" name="sobrenome" id="idSobrenome">
+      <input type="submit" value="Enviar"></input>
+    </form>
+  </section>
 </body>
 </html>
